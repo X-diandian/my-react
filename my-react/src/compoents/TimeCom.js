deb529479be6910406305da4db849272aba1c011
xiang ning <920526557@qq.com>
2019-03-13 15:53:18 +0800

    更新时钟组件

---
+++ my-react/src/compoents/TimeCom.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+class TimeCom extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state={   //state用来设置
+            date:new Date()
+        }
+    }
+
+    componentDidMount() {
+        this.timer=setInterval(()=>{
+            this.setState({
+                date:new Date()
+            })
+        },1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer)
+    }
+
+
+    render() {
+            return (
+                <div>
+                    <h1>{this.state.date.toLocaleTimeString()}</h1>
+                </div>
+
+            )
+    }
+}
+
+export default TimeCom
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- my-react/src/welcome.js
+++ my-react/src/welcome.js
@@ -1,5 +1,6 @@
 import  React from 'react' //引入依赖
 import BusinessCard from './compoents/BusinessCard'
+import TimeCom from './compoents/TimeCom'
 class Welcome extends  React.Component{  //继承组件
     render() {
 
@@ -7,6 +8,7 @@ class Welcome extends  React.Component{  //继承组件
         return (
             <div className="App">
                 <BusinessCard />
+                <TimeCom />
                 <h1>dfd{'fff'}{"32424"}+++{3+5}</h1>
                 <h1 id="user">[1,2,3]</h1>
                 {[12,3434]}
